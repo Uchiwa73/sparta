6835fa54e42e0b1bac475ee34df3a071e84438ee
Uchiwa73 <40432200+Uchiwa73@users.noreply.github.com>
2020-03-30 21:32:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -526,4 +526,6 @@ if (command == "warn") {
 
 });
 
-bot.login("NjkzNDYzMzE3Nzc1MTIyNDMz.XoHs-Q.Dg803qXP7WxnDltHT6cr7eEdfZ4")
+bot.login("NjkzNDYzMzE3Nzc1MTIyNDMz.XoHs-Q.Dg803qXP7WxnDltHT6cr7eEdfZ4").catch(err => {
+  console.log(err)
+});
